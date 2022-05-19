80713d2c8f373109405c85c443ea799b6fc63511
LeToyek <maulanatoyek@gmail.com>
2022-05-19 16:41:06 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import TodoList from "./TodoList";
+
+function App() {
+  
+  return (
+    <>
+      <TodoList />
+      <input type="text"/>
+      <button>button1</button>
+      <button>login</button>
+      <div>
+        test
+      </div>
+    </>
+  )
+}
+
+export default App;
