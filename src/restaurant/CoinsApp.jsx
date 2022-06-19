7cbd1f11356b1fa41a9bcbf8949ac804441a37e6
LeToyek <maulanatoyek@gmail.com>
2022-06-19 23:11:47 +0700

    fetch

--- src/restaurant/CoinsApp.jsx
+++ src/restaurant/CoinsApp.jsx
@@ -5,18 +5,37 @@ import './style.scss'
 const RestaurantApp = () => {
   const [coins, setCoins] = useState(null)
 
+  const fetchCoinData = async ()=>{
+    const raw = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+    const data = await raw.json()
+
+    return data
+  }
+
   useEffect(()=>{
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-      .then(res => {
-        return res.json()
+    const populateData = async ()  => {
+      const result = await fetchCoinData()
+      setCoins(result)
+    }
+    
+    populateData()
+
+    window.addEventListener('scroll', e => {
+      console.log(window.scrollY)
+    })
+    
+    // fetchData()
+    // fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    //   .then(res => {
+    //     return res.json()
         
-      })
-      .then(c => {
-        setCoins(c)
+    //   })
+    //   .then(c => {
+    //     setCoins(c)
         
-      }).catch(
-        err => console.log(err)
-      )
+    //   }).catch(
+    //     err => console.log(err)
+    //   )
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
   return (
